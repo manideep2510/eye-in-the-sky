14e9f7354ceeacee1bde99f7952d2abbf72add62
Manideep Kolla <manideep2510@gmail.com>
2018-12-17 05:46:33 +0000

    Testing, accuracies, saving predictions

--- test_unet.py
+++ test_unet.py
@@ -133,7 +133,7 @@ for fname in filelist_trainx[:13]:
     
     crop_size = 128
     
-    stride = 64
+    stride = 32
     
     h, w, c = image.shape
     
@@ -158,7 +158,7 @@ for fname in filelist_trainy[:13]:
     
     crop_size = 128
     
-    stride = 64
+    stride = 32
     
     h, w, c = image.shape
     
@@ -472,7 +472,7 @@ def conf_matrix(Y_gt, Y_pred, num_classes = 9):
 
         conf_matrix = confusion_matrix(gt, pred, labels=[0,1,2,3,4,5,6,7,8])
         
-        kappa = cohen_kappa_score(gt,pred, labels=[0,1,2,3,4,5,6,7,8])
+        kappa = cohen_kappa_score(gt,pred, labels=[0,1,2,3,4,5,6,7])
 
         pixels = len(pred)
         total_pixels = total_pixels+pixels
@@ -481,19 +481,139 @@ def conf_matrix(Y_gt, Y_pred, num_classes = 9):
         
         kappa_sum = kappa_sum + kappa
 
-    final_confusion_matrix = sudo_confusion_matrix/total_pixels
+    final_confusion_matrix = sudo_confusion_matrix
     
     final_kappa = kappa_sum/n
 
     return final_confusion_matrix, final_kappa
 
 confusion_matrix_train, kappa_train = conf_matrix(Y_gt_train_13, pred_train_13, num_classes = 9)
+print('Confusion Matrix for training')
 print(confusion_matrix_train)
+print('Kappa Coeff for training without unclassified pixels')
 print(kappa_train)
 
 confusion_matrix_test, kappa_test = conf_matrix(Y_gt_val, pred_val_all, num_classes = 9)
+print('Confusion Matrix for validation')
 print(confusion_matrix_test)
+print('Kappa Coeff for validation without unclassified pixels')
 print(kappa_test)
+
+
+# Pass Confusion matrix, label to which the accuracy needs to be found, number of classes to be considered
+# Returns that particular class accuracy
+
+def acc_of_class(class_label, conf_matrix, num_classes = 8):
+    
+    numerator = conf_matrix[class_label, class_label]
+    
+    denorminator = 0
+    
+    for i in range(num_classes):
+        denorminator = denorminator + conf_matrix[class_label, i]
+        
+    acc_of_class = numerator/denorminator
+    
+    return acc_of_class
+
+
+# On training
+
+# Find accuray of all the classes NOT considering the unclassified pixels
+
+for i in range(8):
+    acc_of_cl = acc_of_class(class_label = i, conf_matrix = confusion_matrix_train, num_classes = 8)
+    print('Accuracy of class '+str(i) + ' WITHOUT unclassified pixels - Training')
+    print(acc_of_cl)
+
+# Find accuray of all the classes considering the unclassified pixels
+
+for i in range(9):
+    acc_of_cl = acc_of_class(class_label = i, conf_matrix = confusion_matrix_train, num_classes = 9)
+    print('Accuracy of class '+str(i) + ' WITH unclassified pixels - Training')
+    print(acc_of_cl)
+    
+# On validation
+
+# Find accuray of all the classes NOT considering the unclassified pixels
+
+for i in range(8):
+    acc_of_cl = acc_of_class(class_label = i, conf_matrix = confusion_matrix_test, num_classes = 8)
+    print('Accuracy of class '+str(i) + ' WITHOUT unclassified pixels - Validation')
+    print(acc_of_cl)
+
+# Find accuray of all the classes considering the unclassified pixels
+
+for i in range(9):
+    acc_of_cl = acc_of_class(class_label = i, conf_matrix = confusion_matrix_test, num_classes = 9)
+    print('Accuracy of class '+str(i) + ' WITH unclassified pixels - Validation')
+    print(acc_of_cl)
+
+
+# Calulating over all accuracy with and without unclassified pixels
+
+def overall_acc(conf_matrix, include_unclassified_pixels = False):
+    
+    if include_unclassified_pixels:
+        
+        numerator = 0
+        for i in range(9):
+        
+            numerator = numerator + conf_matrix[i,i]
+        
+        denominator = 0   
+        for i in range(9):
+            for j in range(9):
+                
+                denominator = denominator + conf_matrix[i,j]
+                
+        acc = numerator/denominator
+        
+        return acc
+    
+    else:
+        
+        numerator = 0
+        for i in range(8):
+        
+            numerator = numerator + conf_matrix[i,i]
+        
+        denominator = 0   
+        for i in range(8):
+            for j in range(8):
+            
+                denominator = denominator + conf_matrix[i,j]
+                
+        acc = numerator/denominator
+        
+        return acc
+
+
+# Training
+
+# Over all accuracy without unclassified pixels
+
+print('Over all accuracy WITHOUT unclassified pixels - Training')
+print(overall_acc(conf_matrix = confusion_matrix_train, include_unclassified_pixels = False))
+
+# Over all accuracy with unclassified pixels
+
+print('Over all accuracy WITH unclassified pixels - Training')
+print(overall_acc(conf_matrix = confusion_matrix_train, include_unclassified_pixels = True))
+
+# Validation
+
+# Over all accuracy without unclassified pixels
+
+print('Over all accuracy WITHOUT unclassified pixels - Validation')
+print(overall_acc(conf_matrix = confusion_matrix_test, include_unclassified_pixels = False))
+
+# Over all accuracy with unclassified pixels
+
+print('Over all accuracy WITH unclassified pixels - Validation')
+print(overall_acc(conf_matrix = confusion_matrix_test, include_unclassified_pixels = True))
+
+
 
 # Convert decimal onehot encode from prediction to actual onehot code
 
@@ -523,6 +643,7 @@ def dec_to_onehot(pred_all):
     return pred_all_onehot_list
 
 
+
 color_dict = {0: (0, 0, 0),
               1: (0, 125, 0),
               2: (150, 80, 0),
@@ -550,12 +671,60 @@ def onehot_to_rgb(onehot, color_dict):
     return np.uint8(output)
 
 
-# Pred on test and save outputs
+# Pred on train, val, test and save outputs
 
 weights_file = "model_onehot.h5"
 model.load_weights(weights_file)
 
 #y_pred_test_all = []
+
+xtrain_list.append(x_val)
+
+
+for i_ in range(len(xtrain_list)):
+    
+    item = xtrain_list[i_]
+    
+    h,w,c = item.shape
+    
+    item = np.reshape(item,(1,h,w,c))
+    
+    y_pred_train_img = model.predict(item)
+    
+    ba,h,w,c = y_pred_train_img.shape
+    
+    y_pred_train_img = np.reshape(y_pred_train_img,(h,w,c))
+    
+    img = y_pred_train_img
+    h, w, c = img.shape
+        
+    for i in range(h):
+        for j in range(w):
+                
+            argmax_index = np.argmax(img[i,j])
+                
+            sudo_onehot_arr = np.zeros((9))
+                
+            sudo_onehot_arr[argmax_index] = 1
+                
+            onehot_encode = sudo_onehot_arr
+                
+            img[i,j,:] = onehot_encode
+    
+    y_pred_train_img = onehot_to_rgb(img, color_dict)
+
+    tif = TIFF.open(filelist_trainx[i_])
+    image2 = tif.read_image()
+    
+    h,w,c = image2.shape
+    
+    y_pred_train_img = y_pred_train_img[:h, :w, :]
+    
+    imx = Image.fromarray(y_pred_train_img)
+    
+    imx.save("train_predictions/pred"+str(i_+1)+".jpg")
+
+
 
 for i_ in range(len(xtest_list1)):
     
@@ -598,4 +767,4 @@ for i_ in range(len(xtest_list1)):
     
     imx = Image.fromarray(y_pred_test_img)
     
-    imx.save("test_outputs/out"+str(i+1)+".jpg")
+    imx.save("test_outputs/out"+str(i_+1)+".jpg")
