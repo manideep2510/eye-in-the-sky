18ad3adaed2fbbd3b888984157349152688908a4
Manideep Kolla <manideep2510@gmail.com>
2019-01-04 20:01:21 +0530

    Update test_unet.py

--- test_unet.py
+++ test_unet.py
@@ -22,6 +22,13 @@ from scipy.misc import imsave
 from keras import backend as keras
 #%matplotlib inline
 
+def iou(y_true, y_pred, smooth = 100):
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    union = K.sum(y_true,-1) + K.sum(y_pred,-1) - intersection
+    #sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
+    iou_acc = (intersection + smooth) / (union + smooth)
+    return iou_acc
+
 model = UNet()
 
 # To read the images in numerical order
